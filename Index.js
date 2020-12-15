dc7cff3a36789cb5ff0acda39b864d964ff4896f
sseans <41116820+sseans@users.noreply.github.com>
2020-12-14 23:08:35 -0700

    javascript stopwatch

--- Index.js
+++ Index.js
@@ -1,48 +1,91 @@
-console.log("Hello World!");
+// Variables to hold time values
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
-let name = 'Sean'; // String
-console.log(name);
-let age = 30; // number
-let isApproved = false; // Boolean
-let firstName = undefined;
-let selectedColor = null;
+// Define variables to hold 'display' values
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
 
-let interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
+// Variable to hold setInterval function
+let interval = null;
 
-let a = 5;
-let b = 6;
-let result = a + b;
-console.log(result);
-b = 7;
-console.log(b);
+// Variable to hold stopwatch status
+let status = "stopped";
 
-for (let i = 0; i < 5; i++) {
-    
-    console.log('hello');
-    
-    if (i == 3) {
-        console.log('no');
-        
+// Stopwatch function (logic to determine when to increment next value etc.)
+function stopWatch() {
+
+    seconds++;
+
+    // Logic to determine when to increment next value
+    if (seconds/60 === 1) {
+        seconds = 0;
+        minutes++;
+
+        if(minutes/60 === 1) {
+            minutes = 0;
+            hours++;
+        }
     }
-    
+
+    // If seconds/minutes/hours are only 1 digit add a leading 0 to the value
+    if (seconds < 10) {
+        displaySeconds = "0" + seconds.toString();
+    }
+    else {
+        displaySeconds = seconds;
+    }
+
+    if (minutes < 10) {
+        displayMinutes = "0" + minutes.toString();
+    }
+    else {
+        displayMinutes = minutes;
+    }
+
+    if (displayHours <10) {
+        displayHours = "0" + hours.toString();
+    }
+    else {
+        displayHours = hours;
+    }
+
+
+    // Display updated time values to user
+    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+
 }
 
-let headphones = {
-    color: 'black',
-    power: false,
-    charge: 100
-};
 
-if (headphones.power == true) {
+function startStop() {
 
-    for (let i = headphones.charge; i > 0; i--) {
-        console.log(i);
+    if (status === "stopped") {
         
+        // Start the stopwatch by calling the setInterval() function
+        interval = window.setInterval(stopWatch, 1000);
+        document.getElementById('startStop').innerHTML = "stop";
+        status = "started";
     }
-};
+    else {
 
+        window.clearInterval(interval);
+        document.getElementById("startStop").innerHTML = "start";
+        status = "stopped";
 
+    }
 
+}
 
+function reset() {
+    
+    window.clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;  
+    document.getElementById('display').innerHTML = "00:00:00";
+    document.getElementById('startStop').innerHTML = "Start";
+    status = "stopped";
+
+}
