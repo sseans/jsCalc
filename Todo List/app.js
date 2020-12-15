dedf6cdc059381cbef96e81816da624431b15ce2
sseans <41116820+sseans@users.noreply.github.com>
2020-12-15 15:58:53 -0700

    modified app.js, initial javascript code

---
+++ Todo List/app.js
@@ -0,0 +1,12 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+// Functions
+function addTodo(event) {
+    
+}
