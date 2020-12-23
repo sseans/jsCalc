1e34e11f715fb85f35b3d9fbe742fc835d34cb60
sseans <41116820+sseans@users.noreply.github.com>
2020-12-23 16:06:30 -0700

    modded .js for readability

--- IdeaBox/ideaBox.js
+++ IdeaBox/ideaBox.js
@@ -6,18 +6,21 @@ logIn.addEventListener('click', logInInterface)
 
 // Functions
 function logInInterface() {
+    
     //Create Opaque BG Div
     const bg = document.createElement('div');
     bg.classList.add('opacityScreen');
     document.body.appendChild(bg);
-    //Create Login Card
+
+    //Create Login Card & Profile Picture
     const card = document.createElement('div');
     card.classList.add('logInCard');
     bg.appendChild(card);
-    //Create Login Profile Picture
+    
     const profilePic = document.createElement('div');
     profilePic.classList.add('profilePic');
     card.appendChild(profilePic)
+
     //Create Login Fields & Welcome Text
     const logInField = document.createElement('div');
     logInField.classList.add('logInField');
